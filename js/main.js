d37fee23661bbc1cfe409d2481f0774bbb3abb9c
Atsune MOGI <77053345+AtsuneMogi@users.noreply.github.com>
2022-06-09 22:14:19 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
+let speeching = 0;
 SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const speech = new SpeechRecognition();
-speech.lang = "en-US";
+speech.lang = document.getElementById("languages").value;
 speech.interimResults = true;
 speech.continuous = true;
 
-const select = document.getElementById("select");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
-const main = document.getElementById("main");
+const message = document.getElementById("message");
 const content = document.getElementById("content");
 const green = "#7FFF00";
 const red = "#F08080";
@@ -23,16 +23,7 @@ function changeLang(select) {
     stopSpeeching();
 }
 
-
-speech.onresult = (e) => {
-    var autotext = e.results[0][0].transcript;
-    content.textContent = autotext;
-};
-
-
 function startSpeeching() {
-    speech.start();
-    main.textContent = "Say Something.";
     start.setAttribute("disabled", true);
     stop.removeAttribute("disabled");
 	start.style.background = gray;
@@ -41,11 +32,43 @@ function startSpeeching() {
 
 
 function stopSpeeching() {
-    speech.stop();
-    main.textContent = 'Press "Start".';
-    content.textContent = "";
-    start.removeAttribute("disabled");
-    stop.setAttribute("disabled", true);
-    start.style.background = green;
-    stop.style.background = gray;
+    if (content != "" || speaking == 0) {
+        message.textcontent = 'Press "Start".';
+        start.removeAttribute("disabled");
+        stop.setAttribute("disabled", true);
+        start.style.background = green;
+        stop.style.background = gray;
+        speech.stop();
+    }
+}
+
+
+function main() {
+    message.textContent = "Say Something."
+    startSpeeching();
+    speech.onsoundstart = function() {
+        speech.start();
+    };
+    speech.onerror = function() {
+        if(speeching == 0) {
+            main();
+        }
+    };
+    speech.onsoundend = function() {
+        main();
+    };
+    speech.onresult = function(e) {
+        var results = e.results;
+        for (var i = e.resultIndex; i < results.length; i++) {
+            if (results[i].isFinal) {
+                content.textContent = results[i][0].transcript;
+                main();
+            } else {
+                content.textContent = results[i][0].transcript;
+                speeching = 1;
+            }
+        }
+    }
+    speeching = 0;
+    speech.start();
 }
