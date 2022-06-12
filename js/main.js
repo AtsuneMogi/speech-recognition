14a75266bd25eb66d219de13e617416e79648234
Atsune MOGI <77053345+AtsuneMogi@users.noreply.github.com>
2022-06-12 16:57:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,16 @@
-let speeching = 0;
-SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const speech = new SpeechRecognition();
-speech.lang = document.getElementById("languages").value;
-speech.interimResults = true;
-speech.continuous = true;
-
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
 const message = document.getElementById("message");
 const content = document.getElementById("content");
-const green = "#7FFF00";
-const red = "#F08080";
-const gray = "#696969";
-start.style.background = green;
-stop.setAttribute("disabled", true);
-stop.style.background = "#666";
-
-
-function changeLang(select) {
-    var lang = select.value;
-    speech.lang = lang;
-    stopSpeeching();
-}
-
-function startSpeeching() {
-    start.setAttribute("disabled", true);
-    stop.removeAttribute("disabled");
-	start.style.background = gray;
-    stop.style.background = red;
-}
-
-
-function stopSpeeching() {
-    if (content != "" || speaking == 0) {
-        message.textcontent = 'Press "Start".';
-        start.removeAttribute("disabled");
-        stop.setAttribute("disabled", true);
-        start.style.background = green;
-        stop.style.background = gray;
-        speech.stop();
-    }
-}
-
 
 function main() {
-    message.textContent = "Say Something."
-    startSpeeching();
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var speech = new SpeechRecognition();
+    speech.lang = document.getElementById("languages").value;
+    speech.interimResults = true;
+    speech.continuous = true;
+    document.getElementById("languages").onchange = function() {
+        main();
+    }
+
     speech.onsoundstart = function() {
         speech.start();
     };
@@ -70,5 +35,6 @@ function main() {
         }
     }
     speeching = 0;
+    message.textContent = "Say Something.";
     speech.start();
 }
